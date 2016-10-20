978453f7792f0acf35ba8c332788c94e1e787483
Berk Ozdilek <biozdilek@gmail.com>
2016-10-20 10:16:05 +0300

    Update simple.js

--- examples/simple.js
+++ examples/simple.js
@@ -6,12 +6,12 @@
 
 var ivencloud = require('ivencloud');
 
-var sk = "your secret key";
-var di = "your device id";
+var sk = "your secret key"; // can be found on Hardware Profile
+var di = "your device uid"; // can be found on devices
 
 // example data
 var data = {
-	key1: 123,
+	key1: 123, // the keys corresbond to the keys on the Hardware Profile
 	key2: "string"
 };
 
