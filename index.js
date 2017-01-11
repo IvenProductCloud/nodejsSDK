beb7e9bea76aac226aab274b0c07e43cd56c61b3
berk ozdilek <biozdilek@gmail.com>
2017-01-11 20:28:10 +0200

    api documentation added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  * ivencloud is a nodejs package for connecting devices to Iven Cloud.
  * Checkout the examples folder to see examples.
  * Further examples and guides can be found in iven blog : http://blog.iven.io
+ * @ignore
  */
 
 // third party dependencies
@@ -21,6 +22,10 @@ const State = {
             case 2: return "ACTIVATED";
         }}};
 
+/**
+* Ivencloud
+* @class
+*/
 var Ivencloud = function() {
   this.uid = "";
   this.secretKey = "";
@@ -30,6 +35,15 @@ var Ivencloud = function() {
   this.hostname = "demo.iven.io";
 };
 
+/**
+ * Sets the credentials.
+ * @param {Object} [creds] - The credentials to be set
+ * @param {string} [creds.hostname=demo.iven.io] - Hostname of the server to be connect
+ * @param {string} [creds.apiKey] - API-KEY of the device
+ * @param {string} creds.deviceUid - Device UId of the device
+ * @param {string} creds.secretKey - Secret Key of the hardware profile of  the device
+ * @memberof Ivencloud
+ */
 Ivencloud.prototype.setCredentials = function(creds) {
     if (creds.hostname) {
       this.hostname = creds.hostname;
@@ -48,6 +62,14 @@ Ivencloud.prototype.setCredentials = function(creds) {
     }
 };
 
+/**
+ * Sends data to the cloud.
+ * @param {Object} [options] - The credentials to be set
+ * @param {string} [options.apiKey] - API-KEY of the device
+ * @param {Object} data - The object that holds the sensor data to be send
+ * @param {Ivencloud~sendDataCallback} callback - Asych. called callback
+ * @memberof Ivencloud
+ */
 Ivencloud.prototype.sendData = function(options, data, callback) {
   if (callback == null && typeof data == 'function') {
     callback = data;
@@ -75,6 +97,14 @@ Ivencloud.prototype.sendData = function(options, data, callback) {
     }
 };
 
+/**
+ * Activates the device and sets the api key
+ * @param {Object} [options] - The credentials to be set
+ * @param {string} [options.deviceUid] - Device UId of the device
+ * @param {string} [options.secretKey] - Secret Key of the hardware profile of  the device
+ * @param {Ivencloud~callback} callback - Asych. called callback
+ * @memberof Ivencloud
+ */
 Ivencloud.prototype.activate = function(options, callback) {
   if (callback == null && typeof options == 'function') {
     callback = options;
@@ -118,6 +148,11 @@ request(reqOpt, function (error, response, body) {
 
 };
 
+/**
+ * Returns the tasks assigned to device
+ * @param {Ivencloud~TasksCallback} callback - Asych. called callback
+ * @memberof Ivencloud
+ */
 Ivencloud.prototype.getTasks = function(callback) {
   this.sendData({FEED:"T"}, function(err, res) {
       if (err) {
@@ -133,8 +168,22 @@ Ivencloud.prototype.getTasks = function(callback) {
         callback(null, ret);
       }
   });
+  /**
+  * Returns the tasks.
+  * @callback Ivencloud~TasksCallback
+  * @param {(Object|null)}      err - return error object in case of error, else null.
+  * @param {(Object|undefined)} res - response from the cloud or nothing in case of error.
+  * @param {number} res.tasCode - task code of the task
+  * @param {string} res.taskValue - value of the task, empty if no value
+  */
 };
 
+/**
+ * Sets the tasks state to complete. Called this after you do the task
+ * @param {number} taskCode - Code of the task completed
+ * @param {Ivencloud~TasksDoneCallback} callback - Asych. called callback
+ * @memberof Ivencloud
+ */
 Ivencloud.prototype.taskDone = function(taskCode, callback) {
   if (callback == null) {
     callback = function(){};
@@ -146,6 +195,13 @@ Ivencloud.prototype.taskDone = function(taskCode, callback) {
       else {
         callback(null,{status:res.status});
       }
+      /**
+      * Returns the tasks.
+      * @callback Ivencloud~TasksDoneCallback
+      * @param {(Object|null)}      err - return error object in case of error, else null.
+      * @param {(Object|undefined)} res - response from the cloud or nothing in case of error.
+      * @param {number} res.status - 200 if successful
+      */
   });
 };
 
@@ -199,152 +255,12 @@ var generateSendDtURL = function (url) {
 };
 
 /**
- * Activate device. Device must be activated to be able to send data
- *
- * @param {String} deviceId
- * @param {String} secretKey
- * @param callback
- * @returns {*}
- */
-Ivencloud.prototype.activate_old = function(deviceId, secretKey, callback){
+* Callback after an request is made to cloud.
+* @callback Ivencloud~callback
+* @param {(Object|null)}      err - return error object in case of error, else null.
+* @param {(Object|undefined)} res - response from the cloud or nothing in case of error.
+* @param {number} res.ivenCode - iven code
+* @param {string} res.api_key - api key of the device
+*/
 
-    if (callback === null | typeof callback !== 'function')
-        return new Error('The third parameter must be valid callback function, please try again!');
-    if (typeof deviceId !== 'string' | typeof secretKey !== 'string')
-        return callback(new Error('Device ID or Secret Key might not be a valid String, please try again!'));
-    if (deviceId === '' | secretKey === '')
-        return callback(new Error('Device ID or Secret Key might be empty, please try again!'));
-
-    var activationCode = cryptoJS.HmacSHA1(deviceId, secretKey);
-    var options = {
-        url: 'http://demo.iven.io/activate/device',
-        headers: {
-            'Activation': activationCode
-        }
-    };
-
-    request(options, function (error, response, body) {
-        if (!error){
-            // check header content-type if json
-            var res = {
-                ivenCode: "",
-                description: "",
-                apiKey: ""
-            };
-            if (response.statusCode < 500) {
-                if (response.headers['content-type'].includes("application/json")) {
-                    // parse json
-                    var info = JSON.parse(body);
-                    if (info.hasOwnProperty('api_key')){
-                        res.apiKey = info.api_key;
-                        // this.apiKey = new String(info.api_key);
-                        ApiKey = info.api_key;
-                    }
-                    if (info.hasOwnProperty('ivenCode'))
-                        res.ivenCode = info.ivenCode;
-                    if (info.hasOwnProperty('description'))
-                        res.description = info.description;
-
-                    callback(res);
-                } else { // content-type is not json
-                    // console.log("no json");
-                    return callback(new Error('The Content-Type is not json, please check again!'));
-                }
-            } else { // responseCode > 500
-                // console.log(response.statusCode);
-                return callback(new Error('The status code was >500, please check again!'));
-            }
-        } else { // error on request
-            // console.log(error);
-            return callback(new Error('The server was not responding with a status code of '+response.statusCode+'. Please check again!'));
-        }
-    });
-};
-
-/**
- * Checks the object if its empty
- *
- * @param obj
- * @returns {boolean} true if empty
- */
-function isEmpty(obj){
-    for (var prop in obj) if (obj.hasOwnProperty(prop)) return false;
-    return true;
-}
-
-/**
- * Sends data to Iven Cloud.
- *
- * @param data
- * @param callback
- * @returns {*}
- */
-Ivencloud.prototype.sendData_old = function (data, callback) {
-
-    if (callback === null | typeof callback !== 'function')
-        return new Error('The second parameter must be valid callback function, please try again!');
-    if (typeof data !== 'object')
-        return callback(new Error('The Api Key provided is not a valid javascript object, please try again!'));
-    if (isEmpty(data))
-       return callback(new Error('The Api Key provided is empty, please try again!'));
-
-
-    if (data | data !== "null" && data !== "undefined")
-    if (ApiKey != "") {
-
-        var body = {data:[]};
-        body.data.push(data);
-        // make the request
-        var options = {
-            method: 'POST',
-            url: 'http://demo.iven.io/data/',
-            headers: {
-                'Content-Type' : 'application/json',
-                'API-KEY': ApiKey
-            },
-            body: JSON.stringify(body)
-        };
-        request(options, function (error, response, body) {
-            if (!error){
-                var res = {
-                    ivenCode: "",
-                    description: "",
-                    apiKey: ""
-                };
-                if (response.statusCode < 500) {
-                    if (response.headers['content-type'].includes("application/json")) {
-                        // parse json
-                        var info = JSON.parse(body);
-                        if (info.hasOwnProperty('api_key'))
-                            res.apiKey = info.api_key;
-                        if (info.hasOwnProperty('ivenCode'))
-                            res.ivenCode = info.ivenCode;
-                        if (info.hasOwnProperty('description'))
-                            res.description = info.description;
-
-                        // call callback and return response object
-                        callback(res);
-                    } else { // content-type is not json
-                        return callback(new Error('The Content-Type is not json, please check again!'));
-                    }
-                } else { // statusCode > 500
-                    // console.log(response.statusCode);
-                  return callback(new Error('The status code was >500, please check again!'));
-                }
-            } else { // error on request
-                // console.log(response.statusCode);
-                return callback(new Error('The server was not responding with a status code of '+response.statusCode+'. Please check again!'));
-            }
-        });
-    } else {
-        // console.log('api key is null');
-        return callback(new Error('The Api Key provided is null, please try again!'));
-
-    }
-};
-
-/**
- *
- * @type {Ivencloud}
- */
 module.exports = new Ivencloud();
